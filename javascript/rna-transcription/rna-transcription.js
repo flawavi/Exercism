c8af6cd08037ee021ec183ed91ac6f68c849234c
Fletcher Watson <fbwthe6@gmail.com>
2017-02-02 15:51:34 -0600

    went from if/else to switch to functional.

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,35 +1,13 @@
 let DnaTranscriber = function() {}
 
-function setCharAt(str,index,chr) {
-  if(index > str.length - 1) return str;
-  return str.substr(0,index) + chr + str.substr(index+1);
+
+DnaTranscriber.prototype.toRna = function(dna) {
+  const codes = {C: 'G', G: 'C', A: 'U', T: 'A'}
+  let rna = dna.split('').map(c => {
+    if (!codes[c]) throw new Error('Invalid input')
+      return codes[c]
+  }).join('')
+  return rna
 }
-
-DnaTranscriber.prototype.toRna = function(nucleotideStrand) {
-  let nucStrand
-
-  for(let i = 0; i <= nucleotideStrand.length - 1; i++){
-    switch (nucleotideStrand.charAt(i)) {
-      case 'C':
-        nucStrand = setCharAt(nucleotideStrand, i, 'G')
-        break
-      case 'G':
-        nucStrand = setCharAt(nucleotideStrand, i, 'C')
-        break
-      case 'A':
-        nucStrand = setCharAt(nucleotideStrand, i, 'U')
-        break
-      case 'T':
-        nucStrand = setCharAt(nucleotideStrand, i, 'A')
-        break
-      default:
-        throw new Error('Invalid input')
-    }
-    return nucStrand
-  }
-}
-
-// let dnaTranscriber = new DnaTranscriber()
-// dnaTranscriber.toRna('C')
 
 module.exports = DnaTranscriber
