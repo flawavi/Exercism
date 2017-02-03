a6117324d53b9a77f5c9f9161ea6994978f860e9
Fletcher Watson <fbwthe6@gmail.com>
2017-02-03 10:57:15 -0600

    changed keys in BOB_SAYS object to be first word of response.

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -3,17 +3,17 @@ const Bob = function() {
   this.hey = function(someoneSays){
 
     const BOB_SAYS = {
-      question: 'Sure.',
-      yell: "Whoa, chill out!",
-      nothing: "Fine. Be that way!",
-      otherwise: "Whatever."
+      sure: 'Sure.',
+      whoa: "Whoa, chill out!",
+      fine: "Fine. Be that way!",
+      whatever: "Whatever."
     }
 
-    if(/^\s*$/.test(someoneSays)) return BOB_SAYS.nothing
-    if(/[A-Z]{4}|[A-Z|0-9]\!$/.test(someoneSays)) return BOB_SAYS.yell
-    if(/[^A-Z]\?$|[K|Y]\?$/.test(someoneSays)) return BOB_SAYS.question
-    if(!someoneSays) return BOB_SAYS.otherwise
-    return BOB_SAYS.otherwise
+    if(someoneSays.match(/^\s*$/)) return BOB_SAYS.fine
+    if(someoneSays.match(/[A-Z]{4}|[A-Z|0-9]\!$/)) return BOB_SAYS.whoa
+    if(someoneSays.match(/[^A-Z]\?$|[K|Y]\?$/)) return BOB_SAYS.sure
+    if(!someoneSays) return BOB_SAYS.whatever
+    return BOB_SAYS.whatever
   }
 }
 
