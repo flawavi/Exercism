3986c6817705954ee3e0a08f6aacdc0af8afa221
Fletcher Watson <fbwthe6@gmail.com>
2017-02-03 10:53:39 -0600

    changed object from RESPOND to BOB_SAYS

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -2,18 +2,18 @@ const Bob = function() {
 
   this.hey = function(someoneSays){
 
-    const RESPOND = {
+    const BOB_SAYS = {
       question: 'Sure.',
       yell: "Whoa, chill out!",
       nothing: "Fine. Be that way!",
       otherwise: "Whatever."
     }
 
-    if(/^\s*$/.test(someoneSays)) return RESPOND.nothing
-    if(/[A-Z]{4}|[A-Z|0-9]\!$/.test(someoneSays)) return RESPOND.yell
-    if(/[^A-Z]\?$|[K|Y]\?$/.test(someoneSays)) return RESPOND.question
-    if(!someoneSays) return RESPOND.otherwise
-    return RESPOND.otherwise
+    if(/^\s*$/.test(someoneSays)) return BOB_SAYS.nothing
+    if(/[A-Z]{4}|[A-Z|0-9]\!$/.test(someoneSays)) return BOB_SAYS.yell
+    if(/[^A-Z]\?$|[K|Y]\?$/.test(someoneSays)) return BOB_SAYS.question
+    if(!someoneSays) return BOB_SAYS.otherwise
+    return BOB_SAYS.otherwise
   }
 }
 
